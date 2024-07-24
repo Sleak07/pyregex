979f387321a8f8d32daa61fc3e7b3d8c2312df49
Bimal <trisky650@gmail.com>
2024-07-24 10:49:07 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Mondrian Art Generator in Python using Al sweigart book
+
+import sys,random,bext
+
+# setting the constants
+
+MIN_X_INCREASE = 6
+MAX_X_INCREASE = 16
+MIN_Y_INCREASE = 3
+MAX_Y_INCREASE = 6
+WHITE = "white"
+BLACK = "black"
+RED = "red"
+BLUE = "blue"
+
+# setting up the screen
+width,height = bext.size()
+
+width = -1
+height = -3
+
+
+
+
+
+
+
